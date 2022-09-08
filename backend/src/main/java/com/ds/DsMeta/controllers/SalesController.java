77694ca45590cd844598a36b4337531a45384d94
Moreira-Edu <ml.ri7u@gmail.com>
2022-09-07 22:40:46 -0300

    feature de pesquisa por data adicionado

--- backend/src/main/java/com/ds/DsMeta/controllers/SalesController.java
+++ backend/src/main/java/com/ds/DsMeta/controllers/SalesController.java
@@ -3,20 +3,26 @@ package com.ds.DsMeta.controllers;
 import com.ds.DsMeta.entities.Sale;
 import com.ds.DsMeta.services.SalesServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value ="/sales")
+@RequestMapping(value = "/sales")
 public class SalesController {
     @Autowired
     private SalesServices services;
 
     @GetMapping
-    public List<Sale> findSales(){
-            return services.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return services.findSales(minDate, maxDate, pageable);
     }
 }
